7d5732e9381f90c5ba85dc8086ba5315a8b30b0d
Gustavo Neves <gustavoneves0607@gmail.com>
2025-11-30 18:16:31 -0300

    Começo da tela do Bolsista

--- lib/Screens/HomePageBolsista.dart
+++ lib/Screens/HomePageBolsista.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
-
+import 'package:gerenciamento_bolsistas/Style/colors.dart';
+import 'package:gerenciamento_bolsistas/Widgets/button.dart';
 class Homepagebolsista extends StatelessWidget{
   const Homepagebolsista({super.key});
   
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      endDrawer: Drawer(
+      backgroundColor: cor2,
+    ),
     appBar: AppBar(
-      title: Text("Olá, coordenador"),
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text("Olá,Bolsista",style: TextStyle(fontFamily: 'ABeeZee',fontSize: 16 ),),
+        ],
+      ),
     ),
-    body: Row(
-      children: [Text('olá')],
+    body:Padding(
+      padding: const EdgeInsets.only(top: 10),
+      child: SizedBox(
+      width: double.infinity,
+      height: double.infinity,
+      child:Column(
+       children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+         children: [
+          Button(text: 'Notificações',onPressed: () {
+          },size: const Size(335,46),color:cor1,icon: Icons.notifications,)
+        ]
+       ),
+       SizedBox(height: 40,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+           Container(
+            height: 140,
+            width:337 ,
+            color: cor1,
+           child: Align(alignment: Alignment.topLeft,
+           child:Text('Metas do projeto') ,),
+           )
+          ],
+        )
+      ],
+      
+    
+    ) ,
     ),
+      ),
     );
+    
     
 
     
