f1a73f4c74965b30edaf88e8bac24f03077d325c
Gustavo Neves <gustavoneves0607@gmail.com>
2025-11-30 18:25:51 -0300

    Botão que sera usado no projeto

--- lib/Style/colors.dart
+++ lib/Style/colors.dart
@@ -1,1 +1,6 @@
 import 'package:flutter/material.dart';
+
+const Color cor1 = Color(0xffA2CFA6);
+const Color cor2 = Color(0xff20BF55);
+const Color cor3 = Color(0xffA8F3B4);
+const Color corLetra1 = Color(0xff000000);
--- lib/Widgets/button.dart
+++ lib/Widgets/button.dart
@@ -1,24 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:gerenciamento_bolsistas/Style/colors.dart';
 
-class MeuBotao extends StatelessWidget {
-  final String texto;
-  final VoidCallback onPressed;
 
-  const MeuBotao({
-    super.key,
-    required this.texto,
-    required this.onPressed,
-  });
+class Button extends StatelessWidget{
+  final String _text;
+  final VoidCallback?  callback;
+  final Size? size;
+  final Color? color;
+  final IconData? icon;
+
+  const Button({Key? key,required String text, VoidCallback? onPressed,this.size,this.color,this.icon})
+      : _text = text,
+       callback = onPressed,
+       super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(255, 33, 243, 51),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-      ),
-      onPressed: onPressed,
-      child: Text(texto),
-    );
+   return ElevatedButton(key: Key(_text),onPressed: callback, 
+   style: ButtonStyle(
+    shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+      RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(25))),
+        backgroundColor: WidgetStateProperty.all<Color>(
+          color ?? cor1,
+        ),
+        fixedSize: WidgetStateProperty.all<Size>(
+          size ?? const Size(23, 26),
+        )
+   ),
+   child: Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      if (icon != null) Icon(icon,color: corLetra1,),             
+      if (icon != null) const SizedBox(width: 8),
+      Text(_text,style: TextStyle(fontFamily: 'ABeeZee',color: corLetra1,),)
+    ],
+   ),
+   );
   }
+
 }
