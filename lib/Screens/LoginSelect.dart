65c89cb895e596421a31ca7d6bab870ec4d6171d
Gustavo Neves <gustavoneves0607@gmail.com>
2025-11-30 18:23:50 -0300

    Tela de login

---
+++ lib/Screens/LoginSelect.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height ,
+        child:
+          Image.asset('assets\Image\Gemini_Generated_Image_56tilv56tilv56ti-removebg-preview 1.png'), 
+      ),
+    );
+  }
+}
