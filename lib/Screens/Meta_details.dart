c8a00b7808012b1782d06bc080c70497158205b2
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-01 15:34:35 -0300

    Tela de detalhes das metas do projeto

---
+++ lib/Screens/Meta_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MetaDetails extends StatelessWidget {
+  const MetaDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      
+    );
+  }
+}
