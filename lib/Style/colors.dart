6eddc28705272ded3f06867a13a6b9953e6a04cf
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-01 16:56:26 -0300

    Cores do projeto

--- lib/Style/colors.dart
+++ lib/Style/colors.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
-const Color cor1 = Color(0xffA2CFA6);
+const Color cor1 = Color(0xffE3FDE7);
 const Color cor2 = Color(0xff20BF55);
 const Color cor3 = Color(0xffA8F3B4);
+const Color cor4 = Color(0xff26BE59);
 const Color corLetra1 = Color(0xff000000);
+const Color corLetra2 = Color(0xffFFFFFF);
