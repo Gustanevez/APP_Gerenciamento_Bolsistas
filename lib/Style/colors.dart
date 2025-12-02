92780ad3387d6e4fd503c436ed25b1ef69e10bd2
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-02 16:21:18 -0300

    Adicionando cor para ser usado no APP

--- lib/Style/colors.dart
+++ lib/Style/colors.dart
@@ -6,3 +6,4 @@ const Color cor3 = Color(0xffA8F3B4);
 const Color cor4 = Color(0xff26BE59);
 const Color corLetra1 = Color(0xff000000);
 const Color corLetra2 = Color(0xffFFFFFF);
+const Color corBorda = Color(0xffA2CFA6);
