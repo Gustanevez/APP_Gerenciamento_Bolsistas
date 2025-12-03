7bf15064b1729f85c8d7797c8598deb37eaa18a8
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-03 15:42:58 -0300

    Gerenciamento de estado dos projetos inseridos no APP

---
+++ lib/Models/Project.dart
@@ -0,0 +1,14 @@
+
+class Project {
+  final String titulo;
+  final DateTime inicio;
+  final DateTime fim;
+  final String area;
+
+  Project({
+    required this.titulo,
+    required this.inicio,
+    required this.area,
+    required this.fim,
+  });
+}
