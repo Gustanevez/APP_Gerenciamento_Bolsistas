7f950c0f11ac5226cddb56baf31eb7aecba0879a
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-03 20:42:16 -0300

    Arquivo que comtém a classe project, que são os projeto do APP

---
+++ lib/Models/Project.dart
@@ -0,0 +1,13 @@
+class Project {
+  final String titulo;
+  final DateTime dataInicio;
+  final DateTime dataFim;
+  final String area;
+
+  Project({
+    required this.titulo,
+    required this.dataInicio,
+    required this.dataFim,
+    required this.area,
+  });
+}
