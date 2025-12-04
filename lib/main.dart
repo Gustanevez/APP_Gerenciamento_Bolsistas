b3d7bf3c991aa5147dbe71df44a98404c319326b
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-03 23:04:08 -0300

    teste

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gerenciamento_bolsistas/Screens/HomePageBolsista.dart';
 import 'package:gerenciamento_bolsistas/Screens/HomePageCoordenador.dart';
 import 'package:gerenciamento_bolsistas/Screens/LoginSelect.dart';
-void main(){
-  runApp(Myapp());
+
+void main() {
+  runApp(ProviderScope(child: Myapp()));
 }
- class Myapp extends StatelessWidget {
+
+class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Login(),
-    );
+    return MaterialApp(home: Login());
   }
 }
