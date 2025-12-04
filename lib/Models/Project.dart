fc234e4ebce88c84d13fe16cd10306c9897d8feb
Gustavo Neves <gustavoneves0607@gmail.com>
2025-12-03 23:03:41 -0300

    teste

--- lib/Models/Project.dart
+++ lib/Models/Project.dart
@@ -2,14 +2,11 @@
 
 class Project {
   final String titulo;
-  final DateTime inicio;
-  final DateTime fim;
   final String area;
 
   Project({
     required this.titulo,
-    required this.inicio,
     required this.area,
-    required this.fim,
+
   });
 }
